4267113acf2c69e696c7b2c7ead836f2b4f6ca59
gadgetvala <surajj.verma7@gmail.com>
2021-03-20 22:14:32 +0530

    Added New Form For add Custom Field

--- 1.Birthday_Remindar/lib/views/dashboard/dashboard.dart
+++ 1.Birthday_Remindar/lib/views/dashboard/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 import './widgets/birthday_header.dart';
 import './../../models/data.dart';
@@ -12,6 +13,125 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   List appData = data;
+  final _formKey = GlobalKey<FormState>();
+
+  String formName;
+  String formAge;
+  String formImageLink;
+
+  Future<void> takeDataFromUser(BuildContext ctx) async {
+    return await showDialog(
+      context: ctx,
+      builder: (builderContext) {
+        return AlertDialog(
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  onSaved: (String value) {
+                    formName = value;
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please provide a name";
+                    }
+
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Name",
+                  ),
+                ),
+                TextFormField(
+                  onSaved: (String value) {
+                    formImageLink = value;
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please provide an image link";
+                    }
+
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Image Link",
+                  ),
+                ),
+                TextFormField(
+                  onSaved: (String value) {
+                    formAge = value;
+                  },
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    RegExp _numeric = RegExp(r'^-?[0-9]+$');
+
+                    if (value.isEmpty) {
+                      return "Please provide age";
+                    }
+
+                    if (!_numeric.hasMatch(value)) {
+                      return "Please Provide valid Number";
+                    }
+
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Age",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                "Save",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xffF289B1),
+                ),
+              ),
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  _formKey.currentState.save();
+
+                  addNewDataToList(
+                      Uuid().v4(), formName, formImageLink, formAge);
+                  Navigator.of(builderContext).pop();
+                }
+              },
+            ),
+            TextButton(
+              child: Text(
+                "Cancle",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(builderContext).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void addNewDataToList(String id, String name, String imageLink, String age) {
+    setState(() {
+      appData.add({
+        'id': id,
+        "name": name,
+        "image": imageLink,
+        "age": age,
+      });
+    });
+  }
 
   void removeDataFromAppData(String id, String method) {
     if (method == "single") {
@@ -91,7 +211,7 @@ class _DashboardState extends State<Dashboard> {
                   ),
                 ),
               ),
-              Controlls(removeDataFromAppData)
+              Controlls(takeDataFromUser, removeDataFromAppData)
             ],
           ),
         ),
