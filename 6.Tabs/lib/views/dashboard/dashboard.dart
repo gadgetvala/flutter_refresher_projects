7c7d3740f44f706c01bdb086147d04e3d4d7f05b
gadgetvala <surajj.verma7@gmail.com>
2021-03-27 14:43:43 +0530

    Inital Commit

---
+++ 6.Tabs/lib/views/dashboard/dashboard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tabs'),
+      ),
+    );
+  }
+}
